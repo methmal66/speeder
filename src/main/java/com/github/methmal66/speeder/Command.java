de4563f0c9772bb26abd823578eb6b242d080c6b
Sanuja Methmal <methmal66@yahoo.com>
2022-08-14 21:50:39 +0530

    add help menu

--- src/main/java/com/github/methmal66/speeder/Command.java
+++ src/main/java/com/github/methmal66/speeder/Command.java
@@ -4,6 +4,7 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.ParseException;
 
 public class Command {
@@ -13,8 +14,21 @@ public class Command {
         final Options options = new Options();
         options.addOption("f", "file", true, "File which containing the text to be displayed");
         options.addOption("w", "wpm", true, "Words Per Minute rate");
+        options.addOption("h", "help", false, "Display help");
+
         final CommandLineParser parser = new DefaultParser();
         this.cmd = parser.parse(options, args);
+        displayHelpMenu(options);
+    }
+
+    private void displayHelpMenu(Options ops) {
+        if (ops.hasOption("h")) {
+            final HelpFormatter formatter = new HelpFormatter();
+            final String header = "Improve your speed reading skill with a simple CLI program";
+            final String footer = "Please report issues at https://github.com/methmal66/speeder";
+            formatter.printHelp("speeder", header, ops, footer);
+            System.exit(0);// exit program successfully
+        }
     }
 
     public String parseFile() {
